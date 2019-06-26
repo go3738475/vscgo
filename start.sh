0fc1135c421ce3ff27481fc221d86d13cb3f73ac
dev <dev@example.com>
2019-06-26 13:23:59 +0300

    add vendor env

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -o pipefail
 export GOROOT=/usr/local/go;
 export GOPATH=/tmp/go;
 export GO111MODULE=on;
+export GOFLAGS=-mod=vendor;
 export PATH=/home/user/bin:$GOPATH/bin:$GOROOT/bin:$PATH;
 
 if [ $(id -u) -eq 1000 ]
