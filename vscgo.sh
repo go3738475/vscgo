2f422d6f11866ab0ee8fed69ac4ec1cf25f2ba97
dev <dev@example.com>
2021-02-05 13:01:11 +0200

    upd paths

--- vscgo.sh
+++ vscgo.sh
@@ -12,7 +12,9 @@ docker run -d \
 --user $(id -u):$(id -g) \
 -p 80:3000 \
 -p 8000:8000 \
--v /home/dev/projects/go/auth:/tmp/go \
+-v /home/dev/projects/go/src:/tmp/go/src/github.com/go3738475 \
+-v /home/dev/projects/go/bin:/tmp/go/bin \
+-v /home/dev/projects/go/pkg:/tmp/go/pkg \
 -v $(pwd)/home:/home/user \
 -v $(pwd)/start.sh:/usr/local/bin/start.sh \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
