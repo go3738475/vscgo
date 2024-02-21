63cefc9b8ccae1ab77ff15d9037d01bcb4c22d4c
dev <dev@example.com>
2024-02-21 18:17:20 +0000

    upd

--- vscgo.sh
+++ vscgo.sh
@@ -9,11 +9,9 @@ xhost local:root;
 # --privileged
 # -v /dev/bus/usb:/dev/bus/usb
 # --security-opt=seccomp:unconfined \
-
-# USB
-# --privileged
-# -v /dev/bus/usb:/dev/bus/usb
-
+# --net host  \
+# --network mynet \
+# -p 8000:8000 \
 
 docker run -d \
 --rm \
@@ -23,7 +21,6 @@ docker run -d \
 -v /home/dev/projects/go/src:/tmp/go/src/github.com/go3738475 \
 -v /home/dev/projects/go/bin:/tmp/go/bin \
 -v /home/dev/projects/go/pkg:/tmp/go/pkg \
--v "/media/dev/7C023A99023A57FA/Users/user/Downloads/Blade Runner (1982) [2160p] [4K] [BluRay] [5.1] [YTS.MX]/:/tmp/v" \
 -v $(pwd)/home:/home/user \
 -v $(pwd)/start.sh:/usr/local/bin/start.sh \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
